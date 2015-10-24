6c5393346ad819816594790d3b9864a8927aeee3
ChristianAbildgaard <cape11@student.aau.dk>
2015-10-24 10:35:38 +0200

    Inverting square matrix
    ## The functions first creates a special matrix, the next function
    calculates the inverse of the original matrix if this hasn't already
    been done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions first creates a special matrix, the next
+## function calculates the inverse of the original matrix if
+## this hasn't already been done
 
-## Write a short comment describing this function
+## This function creates a special matrix which is really a 
+## list containing a function to 1. set the value of the vector
+## 2. get the value of the vector 3. set the value of the inverse
+## 4. get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        I <- NULL
+        set <- function(y) {
+                x <<- y
+                I <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) I <<- inverse
+        getinverse <- function() I
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse of the original matrix,
+## if this hasn't already been done, using the function solve
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        I <- x$getinverse()
+        if (!is.null(I)) {
+                message("getting cached data")
+                return(I)
+        }
+        O_Mat <- x$get()
+        I <- solve(O_Mat, ...)
+        x$setinverse(I)
+        I
 }
